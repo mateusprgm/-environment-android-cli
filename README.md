cd057d8316b715ed4a3981ba9d6bd412a410828d
Mateus Pereira <mateusprgm@gmail.com>
2020-05-24 21:46:40 -0300

    Up Project

---
+++ README.md
@@ -0,0 +1,23 @@
+## Requirements:  
+
+#### JDK: installed (jar dependency for unzip the archives)
+#### Git bash: installed (curl dependency for do download of archives)
+
+## Instructions: 
+#### Exec the bat archive: **execme.bat**  
+
+## This is the flux: 
+
+the script do download of  android-tools and 
+android-platform-tools, 
+unzips the two archives and execute sdkmanager generating 
+the platforms folder and the build-tools folder after created environments variables necessary.
+
+And after this steps run for hug!
+
+FREE OF ANDROID STUDIO.
+Installed or downloaded.
+
+Using only necessary, only cli.
+
+## Tested in Flutter and Ionic 4
---
+++ execme.bat
@@ -0,0 +1,28 @@
+@ECHO
+SETX /M ANDROID_HOME "%cd%"
+
+SETTING ENVIRONMENT VARIABLES
+
+SET BUILDTOOLS_PATH=%cd%\build-tools
+SET TOOLS_PATH=%cd%\build-tools
+SET PLATAFORMTOOLS_PATH=%cd%\build-tools
+SET PLATFORMS_PATH=%cd%\build-tools
+
+SET GROUPTOOLS_PATH=%path%;%BUILDTOOLS_PATH%;%TOOLS_PATH%;%PLATAFORMTOOLS_PATH%;%PLATFORMS_PATH%
+
+SETX /M path "%GROUPTOOLS_PATH%"
+
+
+DOWNLOADING PLATFORMS-TOOLS AND SDK-TOOLS
+
+curl https://dl.google.com/android/repository/platform-tools_r30.0.1-windows.zip -o platform-tools.zip
+curl https://dl.google.com/android/repository/sdk-tools-windows-4333796.zip -o tools.zip
+
+jar xf platform-tools.zip
+jar xf tools.zip
+
+del "%cd%\platform-tools.zip"
+del "%cd%\tools.zip"
+
+"%cd%\tools\bin\/sdkmanager.bat" "platforms;android-28" "platform-tools" "build-tools;28.0.3"
+
